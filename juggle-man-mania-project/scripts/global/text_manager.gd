d7576993564a65c82aa592b2f725afd1d8c18f3e
Isabelle1909 <isabelleisapokemon@gmail.com>
2026-03-29 15:10:35 +1300

    text manager established

---
+++ juggle-man-mania-project/scripts/global/text_manager.gd
@@ -0,0 +1,43 @@
+extends Node
+
+var json_path = "res://data/test_json.json"
+
+var talked_to_nums = {
+	"wardrobe": 0,
+	"mirror": 0,
+	"bed": 0,
+	"tv": 0
+}
+
+var all_text = {}
+
+func _ready():
+	load_json_file()
+	display_text("mirror")
+
+func load_json_file():
+	#open file for reading
+	var file = FileAccess.open(json_path, FileAccess.READ)
+	#check if file exists
+	assert(file.file_exists(json_path), "File path does not exist")
+	
+	# Read the contents of the file as text
+	var json = file.get_as_text()
+	var json_object = JSON.new()
+	
+	#parse the JSON text
+	json_object.parse(json)
+	#Store the parsed data in the content dictionary
+	all_text = json_object.data
+
+func display_text(ob_name):
+	#increments the objects talk_to number
+	talked_to_nums[ob_name] += 1
+		#gets the correct text from the json file depending on object, time/day and number of 
+	#times it's been spoken to
+	var path_string = str(SystemManager.time,"_",SystemManager.day,"_",SystemManager.mood)
+	var text = all_text[ob_name][path_string][str(talked_to_nums[ob_name])]
+	print(text)
+
+	
+	pass
